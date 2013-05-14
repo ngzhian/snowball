bd7fbce875cf0597e87f870b16244bf4f8f4f7bb
Ng Zhi An <ngzhian@gmail.com>
2013-05-14 13:15:37 +0800

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,54 +1,38 @@
 var CANVAS_WIDTH = 480;
 var CANVAS_HEIGHT = 320;
 
-var canvasElement = $("<canvas width='" + CANVAS_WIDTH +
+var canvasElement = jQuery("<canvas width='" + CANVAS_WIDTH +
         "' height='" + CANVAS_HEIGHT + "'></canvas>");
 var canvas = canvasElement.get(0).getContext("2d");
 canvasElement.appendTo('body');
 
 var FPS = 30;
-setInterval(function() {
-    update();
-    draw();
-}, 1000/FPS);
+setInterval(function() { update(); draw(); }, 1000/FPS);
 
-var ball = {
-    x: 100,
-    y: 100,
-    r: 20,
-    draw: function() {
-        canvas.fillStyle = "#dd6565";
-        canvas.fillRect(this.x, this.y, 10, 10);
-    }
-}
+var rollingSpeed = 3;
 
-var environment = {
-    "trees" : [
-    { x: 120, y: 120 }
-        ],
-    draw: function() {
-        for (var i = 0; i < 10; i++) {
-            console.log(environment.trees[i]);
-            canvas.fillStyle = "#3232ee";
-            canvas.fillRect(trees[i].x, trees[i].y, 10, 10);
-        }
-    }
-}
+var trees = []
+trees.push(Tree({x:90, y:10}), Tree({x:200, y:100}));
 
 var delta_x = 5;
 var delta_y = 5;
 
+var camera = Camera({});
+var ball = Ball({});
+
 function update() { 
     if (keydown.left) {
+        camera.x--;
         ball.x--;
     }
     if (keydown.right) {
+        camera.x++;
         ball.x++;
     }
 }
-    environment.draw();
 
 function draw() {
     canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ball.draw();
+    trees.forEach(function(tree) { tree.draw(); });
 }
